2e2a5808c63fdad4dd25b67ee43be66928948f85
webpwnized <webpwnized@gmail.com>
2018-05-31 12:51:24 -0400

    Updated configuration file

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ JTR_EXECUTABLE_FILE_PATH = "/opt/JohnTheRipper/run/john"
 JTR_POT_FILE_PATH = "/opt/JohnTheRipper/run/john.pot"
 
 # Default values for Kali Linux Rolling 2018
+# NOTE: You should install the latest version of JTR. A video tutorial is available at https://www.youtube.com/watch?v=7R10QN_uCh0
 #JTR_EXECUTABLE_FILE_PATH = "/usr/sbin/john"
 #JTR_POT_FILE_PATH = "/root/.john/john.pot"
 
