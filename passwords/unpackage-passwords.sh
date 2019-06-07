235bd89dfc434c8af93eb7655106bbaf40b87451
webpwnized <webpwnized@gmail.com>
2019-06-07 17:53:40 -0400

    Version 1.3.10 Add unpack passwords script

---
+++ passwords/unpackage-passwords.sh
@@ -0,0 +1,4 @@
+cat passwords-hailmary-1.txt.zip passwords-hailmary-2.txt.zip passwords-hailmary-3.txt.zip > passwords-hailmary.txt.zip
+unzip passwords-hailmary.txt.zip
+rm passwords-hailmary.txt.zip
+ls -lah
