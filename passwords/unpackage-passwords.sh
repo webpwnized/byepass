6a48243566bb21dfad11f5da83a11fcdfb020626
webpwnized <webpwnized@gmail.com>
2019-06-13 11:25:21 -0400

    Version 1.5.6 Improved password lists Updated password list packaging scripts

--- passwords/unpackage-passwords.sh
+++ passwords/unpackage-passwords.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
+echo '[*] Concatenating files'
 cat passwords-hailmary-1.txt.zip passwords-hailmary-2.txt.zip passwords-hailmary-3.txt.zip passwords-hailmary-4.txt.zip > passwords-hailmary.txt.zip
+echo '[*] Unzipping passwords-hailmary.txt'
 unzip passwords-hailmary.txt.zip
+echo '[*] Deleting temporary work files'
 rm passwords-hailmary.txt.zip
 ls -lah
