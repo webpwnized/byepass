a740858c4e55c070a86ab7753514e99940fe5c34
webpwnized <webpwnized@gmail.com>
2019-06-13 11:23:25 -0400

    Version 1.5.6 Improved password lists Updated password list packaging scripts

--- passwords/package-passwords.sh
+++ passwords/package-passwords.sh
@@ -9,10 +9,12 @@ mv /tmp/p passwords-hailmary.txt
 echo '[*] Compressing passwords-hailmary.txt'
 zip passwords-hailmary.txt.zip passwords-hailmary.txt
 echo '[*] Splitting passwords-hailmary.txt.zip'
-split -n 3 passwords-hailmary.txt.zip passwords-hailmary-
+split -n 4 passwords-hailmary.txt.zip passwords-hailmary-
 mv passwords-hailmary-aa passwords-hailmary-1.txt.zip
 mv passwords-hailmary-ab passwords-hailmary-2.txt.zip
 mv passwords-hailmary-ac passwords-hailmary-3.txt.zip
+mv passwords-hailmary-ad passwords-hailmary-4.txt.zip
+echo '[*] Deleting temporary working files'
 rm passwords-hailmary.txt.zip
 echo '[*] Passwords have been packaged for GitHub'
 ls -lah
--- passwords/unpackage-passwords.sh
+++ passwords/unpackage-passwords.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
-cat passwords-hailmary-1.txt.zip passwords-hailmary-2.txt.zip passwords-hailmary-3.txt.zip > passwords-hailmary.txt.zip
+cat passwords-hailmary-1.txt.zip passwords-hailmary-2.txt.zip passwords-hailmary-3.txt.zip passwords-hailmary-4.txt.zip > passwords-hailmary.txt.zip
 unzip passwords-hailmary.txt.zip
 rm passwords-hailmary.txt.zip
-rm passwords-hailmary-1.txt.zip
-rm passwords-hailmary-2.txt.zip
-rm passwords-hailmary-3.txt.zip
 ls -lah
