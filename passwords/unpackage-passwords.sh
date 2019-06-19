eb83e014f42243e292e259909f8ad7ff6f4c0768
webpwnized <webpwnized@gmail.com>
2019-06-19 09:06:43 -0400

    Version 1.5.13 Update password packaging scripts to handle larger password sets

--- passwords/unpackage-passwords.sh
+++ passwords/unpackage-passwords.sh
@@ -2,7 +2,7 @@
 echo '[*] Current directory contents'
 ls -lah
 echo '[*] Concatenating files'
-cat passwords-hailmary-1.txt.zip passwords-hailmary-2.txt.zip passwords-hailmary-3.txt.zip passwords-hailmary-4.txt.zip > passwords-hailmary.txt.zip
+cat passwords-hailmary-1.txt.zip passwords-hailmary-2.txt.zip passwords-hailmary-3.txt.zip passwords-hailmary-4.txt.zip passwords-hailmary-5.txt.zip > passwords-hailmary.txt.zip
 echo '[*] Unzipping passwords-hailmary.txt'
 unzip passwords-hailmary.txt.zip
 echo '[*] Deleting temporary work files'
